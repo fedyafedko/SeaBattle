0d6df36734ad49528752c42b18ca48c327a4f147
unknown <vladfedko35@gmail.com>
2023-02-10 13:21:17 +0200

    SeaBattel

---
+++ SeaBattle/SeaBattle/Cell.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattle;
+public class Cell
+{
+    private (int X, int Y) _coordinates;
+    private bool _isShooted;
+}
